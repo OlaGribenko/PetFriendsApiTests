0d1c9534c830f2296a4d4b76b6e11a172f6faddb
olagribenko <olagribenko@gmail.com>
2022-07-12 21:23:57 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,4 @@
+valid_email = "olagribenko@gmail.com"
+valid_password = "Qa-7525150$"
+wrong_valid_email = "olagribenko99@gmail.com"
+wrong_valid_password = "7525150"
---
+++ tests/test_pet_friends.py
@@ -0,0 +1,102 @@
+from api import PetFriends
+from settings import valid_email, valid_password, wrong_valid_email, wrong_valid_password
+import os
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+
+def test_add_new_pet_with_valid_data(name='Зеленка', animal_type='кисуня',
+                                     age='5', pet_photo='images/Cat1.jpg'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == name
+
+
+def test_successful_update_self_pet_info(name='Зеленка', animal_type='тигр',
+                                         age='7'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+
+        assert status == 200
+        assert result['name'] == name
+    else:
+        raise Exception("There is no my pets")
+
+
+def test_delete_pet_in_empty_list():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) > 0:
+        for i in range(len(my_pets['pets'])):
+            pet_id = my_pets['pets'][i]['id']
+            _ = pf.delete_pet(auth_key, pet_id)
+
+
+def test_get_api_key_for_wrong_valid_user(email=wrong_valid_email, password=wrong_valid_password):
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 200
+
+
+def test_add_new_pet_with_not_valid_data_faild(name='Зеленка', animal_type='кисуня',
+                                               age='5ь', pet_photo='images/Cat1.jpg'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['age'] == age
+
+
+def test_add_new_pet_with_not_valid_data_name(name='@$#%&$*%@#$', animal_type='кисуня',
+                                              age='5', pet_photo='images/Cat1.jpg'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['age'] == age
+
+
+def test_successful_delete_self_pet():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "@$#%&$*%@#$", "кисуня", "5", "images/Cat1.jpg")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    assert status == 200
+    assert pet_id not in my_pets.values()
+
+
+def test_add_new_pet_with_not_valid_data_photo(name='Зеленка', animal_type='кисуня',
+                                               age='5', pet_photo='images/Cat1.tiff'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['pet_photo'] == pet_photo
